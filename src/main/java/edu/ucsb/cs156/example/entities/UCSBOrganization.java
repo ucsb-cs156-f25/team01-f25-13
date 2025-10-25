6fcf0783958bff96a6e7399feb0b89c193882d39
Jackson Cao <jacksoncao@ucsb.edu>
2025-10-24 23:53:02 -0700

    changed name of UCSBOrganization entity to the correct name

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBOrganization.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBOrganization.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "ucsborganization")
+@Entity(name = "UCSBOrganization")
 public class UCSBOrganization {
   @Id private String orgCode;
   private String orgTranslationShort;
