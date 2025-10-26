0a51cda40a5adec1ec2cde09e0b2c82ff56c3c80
Hao Ding <hao_ding@ucsb.edu>
2025-10-26 03:36:17 -0700

    naming issue fixed

--- src/main/java/edu/ucsb/cs156/example/entities/HelpRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/HelpRequest.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "helprequests")
+@Entity(name = "help_requests")
 public class HelpRequest {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
